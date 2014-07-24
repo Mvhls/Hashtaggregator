a7409f04602ab7570b9405537f8ae21b9aee0c62
Stuart Pearlman & Zach Pflederer <pair+StuartPearlman.zpfled@devbootcamp.com>
2014-07-23 19:06:31 -0500

    failed attempt at getting testing up and running for tweets_controller

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -5,8 +5,11 @@ class TweetsController < ApplicationController
     set_up_client
     @client.filter(:track => "#myworstbirthday") do |object|
       next if object.text.include? 'RT'
-      p object.text if object.is_a?(Twitter::Tweet)
-      # object
+
+      # p object.text if object.is_a?(Twitter::Tweet)
+      # hey mike/jonathon, this seems a little janky to us but it works.
+      @tweet = object
+      return (@tweet) if ENV[:test]
     end
   end
 end
