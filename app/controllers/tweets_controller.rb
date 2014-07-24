54013481914b84ca14b484ca75bfbe39bccf2341
Stuart Pearlman & Zach Pflederer <pair+StuartPearlman.zpfled@devbootcamp.com>
2014-07-23 20:10:01 -0500

    try passing a hash instead of a Twitter::Tweet

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -5,11 +5,20 @@ class TweetsController < ApplicationController
     set_up_client
     @client.filter(:track => "#myworstbirthday") do |twitter_tweet|
       next if twitter_tweet.text.include? 'RT'
-      Backlog.perform_async(twitter_tweet)
+      new_tweet = {
+        username: twitter_tweet.user.screen_name,
+        content: twitter_tweet.full_text,
+        latitude: twitter_tweet.geo.coordinates[0],
+        longitude: twitter_tweet.geo.coordinates[1],
+        url: twitter_tweet.place.url,
+        twitter_id: twitter_tweet.id,
+        location: twitter_tweet.place.full_name
+      }
+      Backlog.perform_async(new_tweet)
       # p twitter_tweet.text if twitter_tweet.is_a?(Twitter::Tweet)
       # hey mike/jonathon, this seems a little janky to us but it works.
-      @tweet = twitter_tweet
-      return (@tweet) if ENV[:test]
+      # @tweet = twitter_tweet
+      # return (@tweet) if ENV[:test]
     end
   end
 end
