4d74713499a29a8fb619ba015324fcf2ce1b13d4
Stuart Pearlman & Zach Pflederer <pair+StuartPearlman.zpfled@devbootcamp.com>
2014-07-24 14:15:27 -0500

    start javascript dev

---
+++ node/Tweet.js
@@ -0,0 +1,12 @@
+function Tweet(args){
+  id: args.id
+  username: args.username
+  content: args.content
+  longitude: args.longitude
+  latitude: args.latitude
+  twitter_id: args.twitter_id
+  location: args.location
+  stars: args.stars
+  created_at: args.created_at
+  updated_at: args.updated_at
+}
