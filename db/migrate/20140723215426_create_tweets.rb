520930978bb87361165a43b7b7ed61c09e713a5f
Stuart Pearlman & Zach Pflederer <pair+StuartPearlman.zpfled@devbootcamp.com>
2014-07-23 20:52:57 -0500

    we have data! background job working

--- db/migrate/20140723215426_create_tweets.rb
+++ db/migrate/20140723215426_create_tweets.rb
@@ -1,14 +1,14 @@
 class CreateTweets < ActiveRecord::Migration
   def change
     create_table :tweets do |t|
-      t.string :username
-      t.string :content
+      t.string  :username
+      t.string  :content
       t.integer :stars
       t.integer :latitude
       t.integer :longitude
-      t.string :url
-      t.integer :twitter_id
-      t.string :location
+      t.string  :url
+      t.string  :twitter_id
+      t.string  :location
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140723215426) do
+ActiveRecord::Schema.define(version: 20140724014600) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,12 +20,11 @@ ActiveRecord::Schema.define(version: 20140723215426) do
     t.string   "username"
     t.string   "content"
     t.integer  "stars"
-    t.integer  "latitude"
-    t.integer  "longitude"
+    t.float    "latitude"
+    t.float    "longitude"
     t.string   "url"
-    t.integer  "twitter_id"
-    t.string   "city"
-    t.string   "state"
+    t.string   "twitter_id"
+    t.string   "location"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
