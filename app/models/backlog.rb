865fb8916ea0a27233aafff44b9f7e48c808450b
Stuart Pearlman & Zach Pflederer <pair+StuartPearlman.zpfled@devbootcamp.com>
2014-07-23 20:15:32 -0500

    activerecord error!!

--- app/models/backlog.rb
+++ app/models/backlog.rb
@@ -6,14 +6,6 @@ class Backlog
   end
 
   def create_tweet(tweet)
-    Tweet.create({
-      username: tweet.user.screen_name,
-      content: tweet.full_text,
-      latitude: tweet.geo.coordinates[0],
-      longitude: tweet.geo.coordinates[1],
-      url: tweet.place.url,
-      twitter_id: tweet.id,
-      location: tweet.place.full_name
-    })
+    Tweet.create(tweet)
   end
 end
