64ed9c1c544034ba88d0ef39a7cd8a96f76a7a63
Zach Pflederer <zpfled@gmail.com>
2014-07-26 10:33:15 -0500

    update filenames

---
+++ query.js
@@ -0,0 +1,37 @@
+var pg = require('pg')
+var conString = (process.env.DATABASE_URL || "postgres://apprentice@localhost/source_development");
+
+module.exports = function(sql, cb) {
+  pg.connect(conString, function(err, client, done) {
+    if(err) {
+      return console.error('error fetching client from pool', err);
+    }
+
+    client.query(sql, function(err, result) {
+      //call `done()` to release the client back to the pool
+      done();
+
+      if(err) return cb(err)
+
+      cb(null, result);
+      //output: 1
+    });
+  });
+}
+
+
+
+if(process.argv[1] === __filename) {
+
+  module.exports({text: 'INSERT INTO "tweets" ("username") VALUES ($1)', values: ['234']}, function(err, data) {
+    if(err) return console.error(err);
+    console.log(data.rows);
+
+    module.exports('SELECT * from tweets', function(err, data) {
+      if(err) return console.error(err);
+      console.log(data.rows);
+    })
+  })
+
+
+}
---
+++ twitterStream.js
@@ -0,0 +1,17 @@
+var keys = require('./keys');
+var Twit = require('twit');
+
+var T = new Twit({
+  consumer_key: process.env.consumer_key,
+  consumer_secret: process.env.consumer_secret,
+  access_token: process.env.access_token,
+  access_token_secret: process.env.access_token_secret
+});
+
+module.exports = function(err, cb) {
+  if(err) return cb(err)
+  var stream = T.stream('statuses/filter', {
+    track: require('./hashtag')
+  });
+  return stream;
+};
